0f869c32216add841d1ee8f985f28fd0dc8824b9
JackHao0921 <jack02131166@gmail.com>
2023-04-10 19:06:40 +0800

    修正全部頁面RWD及首頁按鈕

--- productpage/js/ham.js
+++ productpage/js/ham.js
@@ -1,65 +1,36 @@
+// 取得漢堡列按鈕及導覽列元素
 const menuBtn = document.querySelector(".menu-btn-rwd");
 const navbar = document.querySelector(".navbar-rwd");
 const modalRwd = document.querySelector(".modal-rwd");
+const navbarItems = document.querySelectorAll(".navbar-rwd li");
 
+// 當漢堡列按鈕被點
 menuBtn.addEventListener('click',function(){
   menuBtn.classList.toggle("open");
   modalRwd.classList.toggle("-modal_style");
   navbar.classList.toggle("open");
 
+  // 切換導覽列的開關狀態
   if (navbar.classList.contains("open")) {
-    // 隱藏 body 捲軸
+    // 開啟時固定背景滾動
     document.body.style.overflow = "hidden";
   } else {
-    // 顯示 body 捲軸
+    // 關閉時恢復背景滾動
     document.body.style.overflow = "auto";
   }
 });
 
+// 點黑背景時關閉導覽列
 modalRwd.addEventListener('click',function(){
-  menuBtn.classList.toggle("open");
-  modalRwd.classList.toggle("-modal_style");
-  navbar.classList.toggle("open");
+  menuBtn.classList.remove("open");
+  modalRwd.classList.remove("-modal_style");
+  navbar.classList.remove("open");
 
-  // 顯示 body 捲軸
+  // 關閉時恢復背景滾動
   document.body.style.overflow = "auto";
 });
 
-const navbarItems = document.querySelectorAll(".navbar-rwd li");
-navbarItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    // 移除所有項目的 active class
-    navbarItems.forEach((item) => {
-      item.classList.remove("active");
-    });
-
-    // 為被點擊的項目加上 active class
-    item.classList.add("active");
-  });
-});
-
-
-// // 取得漢堡列按鈕及導覽列元素
-// const menuBtn = document.querySelector(".menu-btn-rwd");
-// const navbar = document.querySelector(".navbar-rwd");
-// const modalRwd = document.querySelector(".modal-rwd");
-// // 當漢堡列按鈕被點
-// menuBtn.addEventListener('click',function(){
-//     // console.log('test')
-//     menuBtn.classList.toggle("open");
-//     modalRwd.classList.toggle("-modal_style");
-  
-//   // 切換導覽列的開關狀態
-//   navbar.classList.toggle("open");
-// })
-
-// modalRwd.addEventListener('click',function(){
-//   menuBtn.classList.toggle("open");
-//   modalRwd.classList.toggle("-modal_style");
-//   navbar.classList.toggle("open");
-// });
-
-// const navbarItems = document.querySelectorAll(".navbar-rwd li");
+// 導覽列項目被點時的樣式
 // navbarItems.forEach((item) => {
 //   item.addEventListener("click", () => {
 //     // 移除所有項目的 active class
